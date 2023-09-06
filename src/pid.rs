c9cb8af01b4880b4799b236040fa6c568bf0f0fd
Isaac Turner <spacey_sooty@outlook.com>
2023-09-06 13:04:22 +0800

    feat documentation

--- src/pid.rs
+++ src/pid.rs
@@ -1,3 +1,11 @@
+/*
+ *  @name Config: the configuration struct for a PID controller
+ *  @param p: the proportion value the PID loop
+ *  @param i: the integral value of the PID loop
+ *  @param d: the derivative value of the PID loop
+ *  @param izone: the distance in which the loop will begin slowing to increase accuracy
+ *  @param error: the acceptable error margin of the loop
+ */
 pub struct Config {
     p: f32,
     i: f32,
@@ -6,20 +14,45 @@ pub struct Config {
     error: f32,
 }
 
+/*
+ *  @name Default: an implementation of the default trait for the Config struct
+ */
 impl Default for Config {
+    /*
+     *  @name default: the default values
+     */
     fn default() -> Self { return Config::from(1.0, 1.0, 1.0, 1.0, 1.0); }
 }
 
+/*
+ *  @name Config methods
+ */
 impl Config {
-    // getters
+    /*
+     *  @name get_p: returns the p value
+     */
     pub fn get_p(&self) -> f32 { return self.p; }
+    /*
+     *  @name get_i: returns the i value
+     */
     pub fn get_i(&self) -> f32 { return self.i; }
+    /*
+     *  @name get_d: returns the d value
+     */
     pub fn get_d(&self) -> f32 { return self.d; }
+    /*
+     *  @name get_izone: returns the izone value
+     */
     pub fn get_izone(&self) -> f32 { return self.izone; }
+    /*
+     *  @name get_error: returns the error margin value
+     */
     pub fn get_error(&self) -> f32 { return self.error; }
 
-    // method to create a new struct
-    pub fn from(p: f32, i: f32, d: f32, izone: f32, error: f32) -> Config {
+    /*
+     *  @name from: the function to create a new pid controller instance
+     */
+    pub fn from(p: f32, i: f32, d: f32, izone: f32, error: f32) -> Self {
         return Config {
             p: p,
             i: i,
